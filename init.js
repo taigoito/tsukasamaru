21a31f7a62f77e1bda6880ec07e46f08a929cbd7
Taigo Ito <webmaster@qwel.design>
2025-03-10 23:04:53 +0900

    Add footer contents

--- init.js
+++ init.js
@@ -4,6 +4,15 @@
  * Location: Fukui, Japan
  */
 
+(() => {
+  const elem = document.querySelector('.footer__copyright');
+  if (!elem) return;
+  const today = new Date();
+  const year = today.getFullYear();
+  elem.innerHTML = `&copy; 2020 - ${year} 海辺の農園宿 つかさ丸`;
+  
+})();
+
 // Back To Top
 new BackToTop();
 
